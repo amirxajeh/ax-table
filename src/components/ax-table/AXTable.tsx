b438fc0bc9579258d048e307e15ac1e0ea065e3e
amir hussein khajeh <axajeh@gmail.com>
2022-01-27 21:39:33 +0330

    add basic table styles

--- src/components/ax-table/AXTable.tsx
+++ src/components/ax-table/AXTable.tsx
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
 
+import './AXTable.scss'
+
 class AXTable extends Component {
 
 
   render() {
     return (
-      <></>
+      <div className="ax-table-container">
+        <table className="ax-table">
+
+          <thead className="table-head">
+            <tr >
+              <td>name</td>
+              <td>age</td>
+              <td>family</td>
+            </tr>
+
+          </thead>
+
+          <tbody className="table-body">
+            <tr>
+              <td>amir</td>
+              <td>23</td>
+              <td>xajeh</td>
+            </tr>
+
+            <tr>
+              <td>sara</td>
+              <td>25</td>
+              <td>xajeh</td>
+            </tr>
+
+            <tr>
+              <td>coco</td>
+              <td>3</td>
+              <td>chanel</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
     )
   }
 }
